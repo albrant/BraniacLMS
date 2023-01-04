954613eebf4a7862bec7a6a0ee91617191b8a193
Evgeniy Albrant <skizee@yandex.ru>
2023-01-04 23:40:57 +0700

    hello world

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
 
-# Create your views here.
+class HelloWorldView(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse("Hello, world!")
+
+
+def hello_world(request):
+    return HttpResponse("Hello, world!")
+
+
+def check_kwargs(request, **kwargs):
+    return HttpResponse(f"kwargs:<br>{kwargs}")
