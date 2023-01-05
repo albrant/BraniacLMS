db67d85aa36c9e3f1d1e9a25784943a58ff12d8b
Evgeniy Albrant <skizee@yandex.ru>
2023-01-05 13:03:49 +0700

    part3 conditions in templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -9,11 +9,12 @@ class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
 
     def get_context_data(self, **kwargs):
-        # get all previous data
+        # Get all previous data
         context = super().get_context_data(**kwargs)
-        # create your own data
+        # Create your own data
         context["news_title"] = "Громкий новостной заголовок"
-        context["news_preview"] = "Предварительное описание, которое заинтересует каждого человека"
+        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        context["range"] = range(5)
         return context
 
 
