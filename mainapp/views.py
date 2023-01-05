9faed80925bdcc9585666d6e9d68dd173b578ef0
Evgeniy Albrant <skizee@yandex.ru>
2023-01-05 11:47:14 +0700

    part 3 - templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -8,6 +8,14 @@ class MainPageView(TemplateView):
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
 
+    def get_context_data(self, **kwargs):
+        # get all previous data
+        context = super().get_context_data(**kwargs)
+        # create your own data
+        context["news_title"] = "Громкий новостной заголовок"
+        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        return context
+
 
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
